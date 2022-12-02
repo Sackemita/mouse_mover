829f7b4ce371ab075adbc3347a2ac5e72a1bf6d8
Sackemita <117820243+Sackemita@users.noreply.github.com>
2022-12-02 11:43:57 +0100

    Add files via upload
    added new method (timer)

--- functions.py
+++ functions.py
@@ -13,6 +13,12 @@ def utworzenie_okna():
     def click_mousemover():  #funkcja która odpala się po kliknięciu buttona (ruszanie myszka)
         print("wow funkcja działa")
 
-    click_button=Button(window, text="Ruszanie myszki", width=15, height=5, command=click_mousemover)
-    click_button.pack()
+    def click_timer():
+        print("timer działa")
+
+    click_button_mouse_mover=Button(window, text="Ruszanie myszki", width=15, height=5, command=click_mousemover)
+    click_button_timer=Button(window, text="Timer", width =15, height=5, command=click_timer)
+
+    click_button_mouse_mover.pack()
+    click_button_timer.pack()
     window.mainloop()
